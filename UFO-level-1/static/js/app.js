bb56883bf297c010ea976844c942d80f7fee423c
nt1983 <73156205+nt1983@users.noreply.github.com>
2021-03-04 23:03:32 -0800

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,12 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody=d3.select("tbody");
+tableData.forEach(function(ufodata){
+    console.log(ufodata);
+    var row=tbody.append("tr");
+    Object.entries(ufodata).forEach(function([key, value]){
+        var cell=row.append("td");
+        cell.text(value);
+    });
+});
