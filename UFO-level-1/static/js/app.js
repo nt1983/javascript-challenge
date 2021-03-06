a4926103f2c965bc93454c9f2d51cbede1b00267
nt1983 <73156205+nt1983@users.noreply.github.com>
2021-03-06 00:21:08 -0800

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,3 +11,26 @@ tableData.forEach(function(ufodata){
         cell.text(value);
     });
 });
+
+var button=d3.select("#filter-btn");
+button.on("click",runenter);
+
+function runenter(){
+    d3.event.preventDefault();
+    var inputElement=d3.select("#datetime");
+    var inputValue=inputElement.property("value");
+    console.log(inputValue, typeof inputValue);
+    var filterdata=tableData.filter(filterValue => filterValue.datetime===inputValue);
+    d3.selectAll("tr").remove();
+    console.log(filterdata);
+    filterdata.forEach(function(ufodata){
+        console.log(ufodata);
+        var row=tbody.append("tr");
+        Object.entries(ufodata).forEach(function([key, value]){
+            var cell=row.append("td");
+            cell.text(value);
+        });
+
+    });
+
+};
